92959685758a1010b8b292c8007db6dc86d1610f
l33klin <l33klin@gmail.com>
2022-03-12 00:05:51 +0800

    init 590

---
+++ leetcode-cn/590/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+
+Authors: klin
+Email: l33klin@foxmail.com
+Date: 3/12/22
+"""
+from typing import List
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        pass
